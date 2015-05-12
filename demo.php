bf6a12648710ad5a61463753b2c9c7a2fe93007c
Niculin Fürst <nf@clickwerk.ch>
2015-05-12 11:08:29 +0200

    Lorrx Captcha Generator upload

---
+++ demo.php
@@ -0,0 +1,61 @@
+<?php
+  /*
+    Lorrx Captcha Generator
+    Version: 1.0.0
+    Autor: Lorrx
+    Licence: GPL v3
+    Copyright: (c) 2015 Lorrx Software
+  */
+  error_reporting(E_ALL);
+  //File include
+  require_once("lib/captcha.php");
+
+  //Create object from class captcha
+  use lorrx\captcha;
+  $captcha = new captcha(5,48,80,200,5000,10,"font/Ubuntu-R.ttf","tmp/");
+
+  //Set captcha options
+  $captcha->setBgColor(255,255,255); //Set background color (R,G,B)
+  $captcha->setPixelColor(231,118,33); //Set pixel color (R,G,B)
+  $captcha->setLineColor(51,51,51); //Set line color (R,G,B)
+  $captcha->setFontColor(51,51,51); //Set font color (R,G,B)
+
+  //Captcha image rendering
+  echo "
+    <h1>Lorrx Captcha Generator</h1>
+    <strong>Captcha:</strong><br />
+    <img src='".$captcha->getCaptcha()."'><br /><br />
+    
+    <form action='' method='post'>
+      <label for='captcha'>Captcha Code:</label>
+      <input type='text' name='code' vlaue='' autofocus />
+      <input type='hidden' name='key' value='".$captcha->getHash("sha1")."' />
+      <input type='submit' name='checkIT' value='Check captcha' />
+    </form>
+  ";
+  if(isset($_POST["checkIT"]))
+  {
+    if(sha1($_POST["code"]) == $_POST["key"]) //or MD5 or crypt
+    {
+      echo "Code is correct!!!";
+    }
+    else
+    {
+      echo "Code is NOT corryect!!!";
+    }
+  }
+  else
+  {
+    echo "
+      <hr />
+      <strong>SHA1 hash:</strong><br />
+      ".$captcha->getHash("sha1")."
+      <br /><br />
+      <strong>Crypt hash:</strong><br />
+      ".$captcha->getHash("crypt")."
+      <br /><br />
+      <strong>MD5 hash:</strong><br />
+      ".$captcha->getHash("md5")."
+    ";
+  }
+?>
---
+++ lib/captcha.php
@@ -0,0 +1,184 @@
+<?php
+  /*
+    Lorrx Captcha Generator
+    Version: 1.0.0
+    Autor: Lorrx
+    Licence: GPL v3
+    Copyright: (c) 2015 Lorrx Software
+  */
+  namespace lorrx
+  {
+    class captcha
+    {
+      private $bgColor;
+      private $image;
+      private $fontSize;
+      private $lineColor;
+      private $pixelColor;
+      private $fontColor;
+      private $fontPath;
+      private $captchaPath;
+      private $lineNumb;
+      private $pixelNumb;
+      private $h;
+      private $w;
+      private $salt;
+      private $clearCacheOnRender;
+      private $textMarginLeft;
+      private $textMarginTop;
+      private $secureWord;
+
+      public function __construct($length, $fontSize, $h=50, $w=150, $PN=500, $LN=5, $fontPath="", $captchaPath="", $MarginTop=0, $MarginLeft=0, $clearCacheOnRender=true)
+      {
+        $this->secureWord = $this->generateString($length);
+        $wordLen = strlen($this->secureWord);
+        $this->image = imagecreatetruecolor($w, $h);
+        $this->h = $h;
+        $this->w = $w;
+        $this->fontSize = $fontSize;
+        $this->fontPath = $fontPath;
+        $this->captchaPath = $captchaPath;
+        $this->pixelNumb = $PN;
+        $this->lineNumb = $LN;
+        $this->setBgColor();
+        $this->setLineColor();
+        $this->setPixelColor();
+        $this->setFontColor();
+        $this->salt = md5($this->generateString($length+25));
+        $this->clearCacheOnRender = $clearCacheOnRender;
+
+        if(!$MarginTop > 0)
+        {
+          $this->textMarginTop = ($h-($h/10));
+        }
+        else $this->textMarginTop = $MarginTop;
+        
+        if(!$MarginLeft > 0)
+        {
+          $this->textMarginLeft = ($w/($wordLen+$h));
+        }
+        else $this->textMarginLeft = $MarginLeft;
+      }
+
+      public function getCaptcha()
+      {
+        $this->renderCaptcha($this->h,$this->w);
+        if($this->clearCacheOnRender)
+        {
+          $this->clearCache();
+        }
+        return $this->captchaPath.$this->salt.".png";
+      }
+
+      public function getHash($cryptType="sha1")
+      {
+        switch ($cryptType) 
+        {
+          case 'sha1':
+            $secure = sha1($this->secureWord);
+            break;
+          case "crypt":
+            $secure = crypt($this->secureWord);
+            break;
+          case "md5":
+            $secure = md5($this->secureWord);
+            break;
+          default:
+            $secure = $this->secureWord;
+            break;
+        }
+        return $secure;
+      }
+
+      public function setFontPath($font)
+      {
+        $this->fontPath = $font;
+      }
+
+      public function setTempPath($temp)
+      {
+        $this->captchaPath = $temp;
+      }
+
+      public function setBgColor($r=255,$g=255,$b=255)
+      {
+        $this->bgColor = imagecolorallocate($this->image, $r, $b, $g);
+      }
+
+      public function setPixelColor($r=204,$g=101,$b=30)
+      {
+        $this->pixelColor = imagecolorallocate($this->image, $r, $b, $g);
+      }
+
+      public function setLineColor($r=34,$g=34,$b=34)
+      {
+        $this->lineColor = imagecolorallocate($this->image, $r, $b, $g); 
+      }
+
+      public function setFontColor($r=34,$g=34,$b=34)
+      {
+        $this->fontColor = imagecolorallocate($this->image, $r, $b, $g);
+      }
+
+      public function setPixel($numb)
+      {
+        $h = $this->h;
+        $w = $this->w;
+        for($i=0;$i<$numb;$i++) 
+        {
+          imagesetpixel($this->image,rand()%$w,rand()%$h,$this->pixelColor);
+        }
+      }
+
+      public function setLine($numb)
+      {
+        $h = $this->h;
+        $w = $this->w;
+        for($i=0;$i<$numb;$i++) 
+        {
+          imageline($this->image,0,rand()%$h,$w,rand()%$h,$this->lineColor);
+        }
+      }
+
+      private function renderCaptcha()
+      {
+        $h = $this->h;
+        $w = $this->w;
+        imagefilledrectangle($this->image,0,0,$w,$h,$this->bgColor);
+        $this->setPixel($this->pixelNumb);
+        $this->setLine($this->lineNumb);
+        $wordLen = strlen($this->secureWord);
+        $a = Array(1,2,3,4,5,6);
+        $arand = array_rand($a);
+        ImageTTFText ($this->image, $this->fontSize, $a[$arand], $this->textMarginLeft, $this->textMarginTop, $this->fontColor, $this->fontPath, $this->secureWord);
+        imagepng($this->image, $this->captchaPath.$this->salt.".png");
+      }
+
+      public function clearCache()
+      {
+        if ($handle = opendir($this->captchaPath)) 
+        {
+          while (false !== ($entry = readdir($handle))) 
+          {
+            if (($entry != ".") AND ($entry != "..") AND ($entry != $this->salt.".png")) 
+            {
+              @unlink($this->captchaPath.$entry);
+            }
+          }
+          closedir($handle);
+        }
+      }
+
+      private function generateString($length) 
+      {
+        $characters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        $randomString = "";
+        for ($i = 0; $i < $length; $i++) 
+        {
+          $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+        return $randomString;
+      }
+    }
+  }
+?>
